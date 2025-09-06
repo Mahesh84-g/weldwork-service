3d26d8758d3dd9069efb39773158d6293ddf2377
Mahesh84-g <maheshreddyvangala84@gmail.com>
2025-09-06 20:12:14 +0530

    Add files via upload

---
+++ weld.js
@@ -0,0 +1,10 @@
+function closebar(){
+      let close_bar=document.querySelector('.sidebar')
+      close_bar.style.display='none'
+
+    }
+    function openbar(){
+      let close_bar=document.querySelector('.sidebar')
+      close_bar.style.display='flex'
+
+    }
